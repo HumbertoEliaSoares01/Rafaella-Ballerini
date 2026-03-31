50ba9f2628b09ea92b8e076b95237bd18aa6221e
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-03-31 18:16:48 -0300

    Update script.js

--- Curso de JavaScript/Parte 2/script.js
+++ Curso de JavaScript/Parte 2/script.js
@@ -1,7 +1,8 @@
     function adicionarTarefa() {
-        //let mensagem = "Tarefa adicionada com sucesso!"            
-        let inputTarefa = document.getElementById("inputTarefa")
-        let tarefa = inputTarefa.value
-        document.getElementById("mensagem").textContent = tarefa;
-        inputTarefa.Value = ""
+        let mensagem = "Tarefa adicionada com sucesso!";
+        document.getElementById("mensagem").textContent = mensagem;
+        
+        let inputTarefa = document.getElementById("inputTarefa");
+
+        console.log(tarefa);
     }
