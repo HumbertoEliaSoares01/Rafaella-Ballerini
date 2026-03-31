0b7a3eee3d79eecef0f728a902506c6717a4cb6c
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-03-31 12:00:21 -0300

    Create script.js

---
+++ Curso de JavaScript/Parte 2/script.js
@@ -0,0 +1,5 @@
+    function adicionarTarefa() {
+        let mensagem = "Tarefa adicionada com sucesso!";
+            document.getElementById("mensagem").textContent = mensagem;
+        let inputTarefa = document.getElementById("inputTarefa")
+    }
