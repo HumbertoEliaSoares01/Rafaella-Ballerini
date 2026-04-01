6e047aceb4ed8c2d2f9576e2b8ff4d854cbaa8e2
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-04-01 18:09:20 -0300

    Update script final.js

--- Curso de JavaScript/Parte 2/script final.js
+++ Curso de JavaScript/Parte 2/script final.js
@@ -2,9 +2,10 @@ function adicionarTarefa() {
     
     let mensagem = "Tarefa adicionada com sucesso!";
     document.getElementById("mensagem").textContent = mensagem;
+    
     let inputTarefa = document.getElementById("inputTarefa")
-
-
+    let tarefa = inputTarefa.value
+    console.log(tarefa)
     
     /* variável inputTarefa recebe valor do input do usuário
     let inputTarefa = document.getElementById("inputTarefa")
