7973f7536538998bca465471c1090cd5a7f1c35f
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-04-02 15:53:26 -0300

    Update script.js

--- Curso de JavaScript/Parte 2/script.js
+++ Curso de JavaScript/Parte 2/script.js
@@ -1,8 +1,10 @@
-    function adicionarTarefa() {
-        let mensagem = "Tarefa adicionada com sucesso!";
-        document.getElementById("mensagem").textContent = mensagem;
-        
-        let inputTarefa = document.getElementById("inputTarefa");
 
-        console.log(tarefa);
-    }
+function adicionarTarefa() {
+    // let mensagem = "Tarefa adicionada com sucesso!";    
+
+    let inputTarefa = document.getElementById("inputTarefa");
+    let tarefa = inputTarefa.value;
+    document.getElementById("mensagem").textContent = tarefa;
+
+    inputTarefa.value = "";
+}
