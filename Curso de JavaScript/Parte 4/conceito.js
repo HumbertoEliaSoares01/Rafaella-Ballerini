66f74042017e2d93ece898546f12c7c796064c95
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-04-07 11:12:17 -0300

    Update conceito.js

--- Curso de JavaScript/Parte 4/conceito.js
+++ Curso de JavaScript/Parte 4/conceito.js
@@ -6,4 +6,4 @@ let frutas = ["maçã ","banana ","mamão "]
 //alert(frutas)
 let mensagem = "O índice da banana " + "é " + frutas.indexOf("banana")
 alert(mensagem)
-alert(frutas)
+alert("o tamanho do vetor [" + frutas + "] é " + frutas.length)
