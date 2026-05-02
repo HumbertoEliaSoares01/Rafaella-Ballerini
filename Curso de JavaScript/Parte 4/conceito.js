fc705b217d6b6ce1b096122e04179f88949e69a5
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-05-02 09:02:37 -0300

    Update conceito.js

--- Curso de JavaScript/Parte 4/conceito.js
+++ Curso de JavaScript/Parte 4/conceito.js
@@ -1,12 +1,12 @@
-//let fruta = "maçã"
-//alert(fruta)
-//fruta = "banana"
-//alert(fruta)
-let frutas = ["maçã ","banana ","mamão "]
-//alert(frutas)
-let mensagem = "O índice da banana " + "é " + frutas.indexOf("banana")
+let fruta = "maçã"
+alert("O valor da variável 'fruta' é "+ fruta)
+fruta = "banana"
+alert("O valor da variável 'fruta' mudou para "+ fruta)
+let frutas = [" maçã ","banana ","mamão "]
+alert("Criando o vetor ["+ frutas+"]")
+let mensagem = "O índice da banana no vetor ["+frutas+"] é " + frutas.indexOf("banana")
 alert(mensagem)
 alert("o tamanho do vetor [" + frutas + "] é " + frutas.length)
 frutas.push("melancia")
 alert("adicionando um ítem ao vetor [" +frutas+ "]")
-alert("o tamanho do vetor agora é " + frutas.length)
+alert("o tamanho do vetor ["+frutas+"] agora é " + frutas.length)
