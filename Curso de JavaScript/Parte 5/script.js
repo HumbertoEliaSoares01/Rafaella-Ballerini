b58794f1e5c04de418f18dd19705693dcc6edff8
HumbertoEliaSoares01 <humbertoelia01@gmail.com>
2026-05-04 14:36:50 -0300

    Create script.js

---
+++ Curso de JavaScript/Parte 5/script.js
@@ -0,0 +1,50 @@
+
+let tarefas = []
+
+// funcção adicionarTarefa acionada pelo evento clique no botão
+function adicionarTarefa() {
+
+    // recebe valor do input do usuário 
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    const mensagem = document.getElementById("mensagem")
+
+    // Se o valor do input for vazio, mostra uma mensagem de erro
+    if (tarefa == ""){
+        // mensagem de erro
+        let mensagemErro = "Digite uma tarefa para adicionar à lista."    
+        mensagem.textContent = mensagemErro
+    } 
+    else{
+        // mensagem de tarefa adicionada com sucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso;
+
+        tarefas.push(tarefa)
+        renderizarTarefas()
+    }
+
+    // limpa o input do usuário    
+    inputTarefa.value = "";
+}
+
+function renderizarTarefas(){
+    // cria novo item (li) e insere da lista (ul)
+    const listaTarefas = document.getElementById("listaTarefas");
+    listaTarefas.innerHTML = "";
+    
+    // for itens na lista
+    // 1. item inicial (iterador)
+    // 2. item final(condição)
+    // 3. se vai de 1 em 1 elemento, ou seja pula (frequência)
+    // for (iterador, condição, frequência)
+
+    let i = 0   // iterador
+
+    for (i; i < tarefas.length; i++ ){
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefas[i]; 
+        listaTarefas.appendChild(novaTarefa);    
+    }
+
+}
